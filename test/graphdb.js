e3e14d1531672b8f681fcf4e279483df75f4f3ad
Victor Felder <victor@draft.li>
2021-04-19 17:12:43 +0200

    feat: ask/construct/describe/select

--- test/graphdb.js
+++ test/graphdb.js
@@ -3,66 +3,136 @@ import { describe, it } from 'mocha'
 import { assert } from 'chai'
 import { GraphDB } from '../lib/graphdb.js'
 
-const randomName = () => `db_${Math.round(Math.random() * 100000000).toString(16)}`
+const randomName = () => `db_${Math.round(Math.random() * 359999964).toString(36)}`
 
 describe('GraphDB', function () {
   this.timeout(Infinity)
 
-  it('should create and drop DB', async () => {
-    const db = new GraphDB()
-    const dbname = randomName()
-    const create = await db.createDb(dbname)
-    const drop = await db.dropDb(dbname)
-    assert.strictEqual(create, true)
-    assert.strictEqual(drop, true)
+  describe('database', function () {
+    it('should create and drop DB', async () => {
+      const db = new GraphDB()
+      const dbname = randomName()
+      const create = await db.createDb(dbname)
+      const drop = await db.dropDb(dbname)
+      assert.strictEqual(create, true, 'createDb failed')
+      assert.strictEqual(drop, true, 'dropDb failed')
+    })
+
+    it('should clear', async () => {
+      const db = new GraphDB()
+      const dbname = randomName()
+      const create = await db.createDb(dbname)
+      const insert = await db.update(dbname, fs.readFileSync('./fixtures/insert.rq').toString())
+      const count = await db.select(dbname, 'select (count(*) as ?tot) where { ?s ?p ?o. }')
+      const inserted = Number(count[0].tot.id.match(/"(\d+)"/)[1])
+
+      const clear = await db.clearDb(dbname)
+
+      setTimeout(async () => {
+        const count2 = await db.select(dbname, 'select (count(*) as ?tot) where { ?s ?p ?o. }')
+        const drop = await db.dropDb(dbname)
+
+        assert.strictEqual(create, true, 'createDb failed')
+        assert.deepStrictEqual(insert, true, 'writing data failed')
+        assert.ok(inserted > 0, 'no data inserted')
+        assert.deepStrictEqual(clear, true, 'clearDb failed')
+        assert.strictEqual(Number(count2[0].tot.id.match(/"(\d+)"/)[1]), 0, 'some data left after clear')
+        assert.strictEqual(drop, true, 'dropDb failed')
+      }, 100)
+    })
+
+    it('should import', async () => {
+      const db = new GraphDB()
+      const dbname = randomName()
+      const create = await db.createDb(dbname)
+
+      const buffer = fs.readFileSync('./fixtures/triples.nt')
+      const insert = await db.import(dbname, buffer)
+
+      const count = await db.select(dbname, 'select (count(*) as ?tot) where { ?s ?p ?o. }')
+      const inserted = Number(count[0].tot.id.match(/"(\d+)"/)[1])
+
+      const clear = await db.clearDb(dbname)
+
+      setTimeout(async () => {
+        const count2 = await db.select(dbname, 'select (count(*) as ?tot) where { ?s ?p ?o. }')
+        const drop = await db.dropDb(dbname)
+
+        assert.strictEqual(create, true, 'createDb failed')
+        assert.deepStrictEqual(insert, true, 'writing data failed')
+        assert.ok(inserted > 0, 'no data inserted')
+        assert.deepStrictEqual(clear, true, 'clearDb failed')
+        assert.strictEqual(Number(count2[0].tot.id.match(/"(\d+)"/)[1]), 0, 'some data left after clear')
+        assert.strictEqual(drop, true, 'dropDb failed')
+      }, 100)
+    })
   })
 
-  it('should clear', async () => {
-    const db = new GraphDB()
-    const dbname = randomName()
-    const create = await db.createDb(dbname)
-    const insert = await db.update(dbname, fs.readFileSync('./fixtures/insert.rq').toString())
-    const count = await db.select(dbname, 'select (count(*) as ?tot) where { ?s ?p ?o. }')
-    const inserted = Number(count[0].tot.id.match(/"(\d+)"/)[1])
+  describe('queries', function () {
+    beforeEach(async function () {
+      this.dbname = randomName()
+      this.db = new GraphDB()
+      await this.db.createDb(this.dbname)
+      await this.db.update(this.dbname, fs.readFileSync('./fixtures/insert.rq').toString())
+    })
 
-    const clear = await db.clearDb(dbname)
+    afterEach(async function () {
+      await this.db.dropDb(this.dbname)
+    })
 
-    setTimeout(async () => {
-      const count2 = await db.select(dbname, 'select (count(*) as ?tot) where { ?s ?p ?o. }')
-      const drop = await db.dropDb(dbname)
+    it('ask', async function () {
+      const success = await this.db.ask(this.dbname, 'ask {<http://example/book1> ?p "A new book"}')
+      assert.strictEqual(success, true, 'ask did not return true')
 
-      assert.strictEqual(create, true)
-      assert.deepStrictEqual(insert, true)
-      assert.ok(inserted > 0)
-      assert.deepStrictEqual(clear, true)
-      assert.strictEqual(Number(count2[0].tot.id.match(/"(\d+)"/)[1]), 0)
-      assert.strictEqual(drop, true)
-    }, 100)
-  })
+      const failure = await this.db.ask(this.dbname, 'ask {<http://example/book1> ?p "nothing"}')
+      assert.strictEqual(failure, false, 'ask did not return false')
+    })
 
-  it('should import', async () => {
-    const db = new GraphDB()
-    const dbname = randomName()
-    const create = await db.createDb(dbname)
+    it('construct', async function () {
+      const result = await this.db.construct(this.dbname, 'CONSTRUCT { <http://favorite-authors> <http://author> ?name } WHERE { ?s <http://purl.org/dc/elements/1.1/creator> ?name }')
 
-    const buffer = fs.readFileSync('./fixtures/triples.nt')
-    const insert = await db.import(dbname, buffer)
+      assert.deepStrictEqual(result, [
+        {
+          termType: 'Quad',
+          value: '',
+          subject: {
+            termType: 'NamedNode',
+            value: 'http://favorite-authors'
+          },
+          predicate: {
+            termType: 'NamedNode',
+            value: 'http://author'
+          },
+          object: {
+            termType: 'Literal',
+            value: 'A.N. Other',
+            language: '',
+            datatype: {
+              termType: 'NamedNode',
+              value: 'http://www.w3.org/2001/XMLSchema#string'
+            }
+          },
+          graph: {
+            termType: 'DefaultGraph',
+            value: ''
+          }
+        }
+      ], 'construct returned unexpected data')
+    })
 
-    const count = await db.select(dbname, 'select (count(*) as ?tot) where { ?s ?p ?o. }')
-    const inserted = Number(count[0].tot.id.match(/"(\d+)"/)[1])
+    it('describe', async function () {
+      const result = await this.db.describe(this.dbname, 'describe <http://example/book1>')
 
-    const clear = await db.clearDb(dbname)
+      assert.deepStrictEqual(result, [
+        { termType: 'Quad', value: '', subject: { termType: 'NamedNode', value: 'http://example/book1' }, predicate: { termType: 'NamedNode', value: 'http://purl.org/dc/elements/1.1/creator' }, object: { termType: 'Literal', value: 'A.N. Other', language: '', datatype: { termType: 'NamedNode', value: 'http://www.w3.org/2001/XMLSchema#string' } }, graph: { termType: 'DefaultGraph', value: '' } },
+        { termType: 'Quad', value: '', subject: { termType: 'NamedNode', value: 'http://example/book1' }, predicate: { termType: 'NamedNode', value: 'http://purl.org/dc/elements/1.1/title' }, object: { termType: 'Literal', value: 'A new book', language: '', datatype: { termType: 'NamedNode', value: 'http://www.w3.org/2001/XMLSchema#string' } }, graph: { termType: 'DefaultGraph', value: '' } }
+      ], 'describe returned unexpected data')
+    })
 
-    setTimeout(async () => {
-      const count2 = await db.select(dbname, 'select (count(*) as ?tot) where { ?s ?p ?o. }')
-      const drop = await db.dropDb(dbname)
+    it('select', async function () {
+      const result = await this.db.select(this.dbname, 'select ?o where { <http://example/book1> <http://purl.org/dc/elements/1.1/creator> ?o . }')
 
-      assert.strictEqual(create, true)
-      assert.deepStrictEqual(insert, true)
-      assert.ok(inserted > 0)
-      assert.deepStrictEqual(clear, true)
-      assert.strictEqual(Number(count2[0].tot.id.match(/"(\d+)"/)[1]), 0)
-      assert.strictEqual(drop, true)
-    }, 100)
+      assert.deepStrictEqual(result, [{ o: { id: '"A.N. Other"' } }], 'select returned unexpected data')
+    })
   })
 })
--- test/stardog.js
+++ test/stardog.js
@@ -3,88 +3,127 @@ import { describe, it } from 'mocha'
 import { assert } from 'chai'
 import { Stardog } from '../lib/stardog.js'
 
-const randomName = () => `db_${Math.round(Math.random() * 100000000).toString(16)}`
+const randomName = () => `db_${Math.round(Math.random() * 359999964).toString(36)}`
 
 describe('stardog', function () {
   this.timeout(Infinity)
 
-  it('should create and drop DB', async () => {
-    const db = new Stardog()
-    const dbname = randomName()
-    const create = await db.createDb(dbname)
-    const drop = await db.dropDb(dbname)
-    assert.ok(create.startsWith('Successfully created database'))
-    assert.ok(drop.includes('was successfully dropped'))
+  describe('database', function () {
+    it('should create and drop DB', async () => {
+      const db = new Stardog()
+      const dbname = randomName()
+      const create = await db.createDb(dbname)
+      const drop = await db.dropDb(dbname)
+      assert.ok(create.startsWith('Successfully created database'), 'createDb failed')
+      assert.ok(drop.includes('was successfully dropped'), 'dropDb failed')
+    })
+
+    it('should clear', async () => {
+      const db = new Stardog()
+      const dbname = randomName()
+      const create = await db.createDb(dbname)
+      const insert = await db.update(dbname, fs.readFileSync('./fixtures/insert.rq').toString())
+      const count = await db.select(dbname, 'select (count(*) as ?tot) where { ?s ?p ?o. }')
+      const inserted = Number(count.results.bindings[0].tot.value)
+      const clear = await db.clearDb(dbname)
+      const count2 = await db.select(dbname, 'select (count(*) as ?tot) where { ?s ?p ?o. }')
+      const drop = await db.dropDb(dbname)
+
+      assert.ok(create.startsWith('Successfully created database'), 'createDb failed')
+      assert.deepStrictEqual(insert, true, 'inserting data failed')
+      assert.ok(inserted > 0, 'no data inserted')
+      assert.deepStrictEqual(clear, { added: 0, removed: inserted }, 'wrong deleted triple count')
+      assert.strictEqual(Number(count2.results.bindings[0].tot.value), 0, 'some data left after clear')
+      assert.ok(drop.includes('was successfully dropped'), 'dropDb failed')
+    })
+
+    it('should import', async () => {
+      const db = new Stardog()
+      const dbname = randomName()
+      const create = await db.createDb(dbname)
+
+      const buffer = fs.readFileSync('./fixtures/triples.nt')
+      const insert = await db.import(dbname, buffer)
+
+      const count = await db.select(dbname, 'select (count(*) as ?tot) where { ?s ?p ?o. }')
+      const inserted = Number(count.results.bindings[0].tot.value)
+      const clear = await db.clearDb(dbname)
+      const count2 = await db.select(dbname, 'select (count(*) as ?tot) where { ?s ?p ?o. }')
+      const drop = await db.dropDb(dbname)
+
+      assert.ok(create.startsWith('Successfully created database'), 'createDb failed')
+      assert.deepStrictEqual(insert, { added: inserted, removed: 0 }, 'wrong inserted triple count')
+      assert.ok(inserted > 0, 'no data inserted')
+      assert.deepStrictEqual(clear, { added: 0, removed: inserted }, 'wrong deleted triple count')
+      assert.strictEqual(Number(count2.results.bindings[0].tot.value), 0, 'some data left after clear')
+      assert.ok(drop.includes('was successfully dropped'), 'dropDb failed')
+    })
+
+    it('should on-offline', async () => {
+      const db = new Stardog()
+      const dbname = randomName()
+      await db.createDb(dbname)
+
+      let error
+      const buffer = fs.readFileSync('./fixtures/triples.nt')
+
+      await db.offline(dbname)
+      try {
+        await db.import(dbname, buffer)
+      }
+      catch (err) {
+        error = err
+      }
+      assert.ok(typeof error !== 'undefined', 'no error when writing to offline DB')
+
+      error = undefined
+      await db.online(dbname)
+      try {
+        await db.import(dbname, buffer)
+      }
+      catch (err) {
+        error = err
+      }
+      assert.ok(typeof error === 'undefined', 'writing to online DB failed')
+
+      await db.dropDb(dbname)
+    })
   })
 
-  it('should clear', async () => {
-    const db = new Stardog()
-    const dbname = randomName()
-    const create = await db.createDb(dbname)
-    const insert = await db.update(dbname, fs.readFileSync('./fixtures/insert.rq').toString())
-    const count = await db.select(dbname, 'select (count(*) as ?tot) where { ?s ?p ?o. }')
-    const inserted = Number(count.results.bindings[0].tot.value)
-    const clear = await db.clearDb(dbname)
-    const count2 = await db.select(dbname, 'select (count(*) as ?tot) where { ?s ?p ?o. }')
-    const drop = await db.dropDb(dbname)
+  describe('queries', function () {
+    beforeEach(async function () {
+      this.dbname = randomName()
+      this.db = new Stardog()
+      await this.db.createDb(this.dbname)
+      await this.db.update(this.dbname, fs.readFileSync('./fixtures/insert.rq').toString())
+    })
 
-    assert.ok(create.startsWith('Successfully created database'))
-    assert.deepStrictEqual(insert, true)
-    assert.ok(inserted > 0)
-    assert.deepStrictEqual(clear, { added: 0, removed: inserted })
-    assert.strictEqual(Number(count2.results.bindings[0].tot.value), 0)
-    assert.ok(drop.includes('was successfully dropped'))
-  })
+    afterEach(async function () {
+      await this.db.dropDb(this.dbname)
+    })
 
-  it('should import', async () => {
-    const db = new Stardog()
-    const dbname = randomName()
-    const create = await db.createDb(dbname)
+    it('ask', async function () {
+      const success = await this.db.ask(this.dbname, 'ask {<http://example/book1> ?p "A new book"}')
+      assert.strictEqual(success, true, 'ask did not return true')
 
-    const buffer = fs.readFileSync('./fixtures/triples.nt')
-    const insert = await db.import(dbname, buffer)
+      const failure = await this.db.ask(this.dbname, 'ask {<http://example/book1> ?p "nothing"}')
+      assert.strictEqual(failure, false, 'ask did not return false')
+    })
 
-    const count = await db.select(dbname, 'select (count(*) as ?tot) where { ?s ?p ?o. }')
-    const inserted = Number(count.results.bindings[0].tot.value)
-    const clear = await db.clearDb(dbname)
-    const count2 = await db.select(dbname, 'select (count(*) as ?tot) where { ?s ?p ?o. }')
-    const drop = await db.dropDb(dbname)
+    it('construct', async function () {
+      const result = await this.db.construct(this.dbname, 'CONSTRUCT { <http://favorite-authors> <http://author> ?name } WHERE { ?s <http://purl.org/dc/elements/1.1/creator> ?name }')
+      assert.strictEqual(result, '<http://favorite-authors> <http://author> "A.N. Other" .', 'construct returned unexpected data')
+    })
 
-    assert.ok(create.startsWith('Successfully created database'))
-    assert.deepStrictEqual(insert, { added: inserted, removed: 0 })
-    assert.ok(inserted > 0)
-    assert.deepStrictEqual(clear, { added: 0, removed: inserted })
-    assert.strictEqual(Number(count2.results.bindings[0].tot.value), 0)
-    assert.ok(drop.includes('was successfully dropped'))
-  })
+    it('describe', async function () {
+      const result = await this.db.describe(this.dbname, 'describe <http://example/book1>')
+      assert.strictEqual(result, `<http://example/book1> <http://purl.org/dc/elements/1.1/title> "A new book" ;
+   <http://purl.org/dc/elements/1.1/creator> "A.N. Other" .`, 'describe returned unexpected data')
+    })
 
-  it('should on-offline', async () => {
-    const db = new Stardog()
-    const dbname = randomName()
-    await db.createDb(dbname)
-
-    let error
-    const buffer = fs.readFileSync('./fixtures/triples.nt')
-
-    await db.offline(dbname)
-    try {
-      await db.import(dbname, buffer)
-    }
-    catch (err) {
-      error = err
-    }
-    assert.ok(typeof error !== 'undefined')
-
-    error = undefined
-    await db.online(dbname)
-    try {
-      await db.import(dbname, buffer)
-    }
-    catch (err) {
-      error = err
-    }
-    assert.ok(typeof error === 'undefined')
-
-    await db.dropDb(dbname)
+    it('select', async function () {
+      const result = await this.db.select(this.dbname, 'select ?o where { <http://example/book1> <http://purl.org/dc/elements/1.1/creator> ?o . }')
+      assert.deepStrictEqual(result.results.bindings, [{ o: { type: 'literal', value: 'A.N. Other' } }], 'select returned unexpected data')
+    })
   })
 })
